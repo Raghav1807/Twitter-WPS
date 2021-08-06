51514d7262cd0315cf3fda1516a7f131f2fcd18b
Raghav1807 <raghav.haralalka@gmail.com>
2021-08-06 19:40:40 +0530

    implemented tabs and add fab list

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,10 +4,37 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
-  }
-];
+    children: [
+      {
+        path: 'firstpage',
+        loadChildren: () =>
+          import('./firstpage/firstpage.module').then((m) => m.FirstpagePageModule),
+      },
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('./search/search.module').then((m) => m.SearchPageModule)
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => 
+          import('./notifications/notifications.module').then( m => m.NotificationsPageModule)
+      },
+      {
+        path: 'messages',
+        loadChildren: () => 
+          import('./messages/messages.module').then( m => m.MessagesPageModule)
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/home/tabs/firstpage',
+    pathMatch: 'full',
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
